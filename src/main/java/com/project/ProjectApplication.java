8bf1fda879808fe4eb1f328f207d443944d05869
Ali <shaik-parvez.ali@capgemini.com>
2023-09-01 10:59:50 +0530

    first commit

---
+++ src/main/java/com/project/ProjectApplication.java
@@ -0,0 +1,18 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+
+}
